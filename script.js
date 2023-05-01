5f1fb9444d9be3b823b76eaaef7dd299a0b4c348
Raffaele Balsamo <80632324+Raffaeleb003@users.noreply.github.com>
2023-05-01 10:34:55 +0200

    area of triangle

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ const sideLength = 9;
 const diagonal = sideLength * Math.sqrt(2);
 
 console.log("The diagonal of a square with a side length of 9 is " + diagonal + ".");
+
+
+
+const sideA = 5;
+const sideB = 6;
+const sideC = 7;
+
+const s = (sideA + sideB + sideC) / 2;
+const area = Math.sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
+
+console.log(`The area of a triangle with sides of length ${sideA}, ${sideB}, and ${sideC} is ${area}.`);
