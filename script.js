063377d640550f59c44384236520cd75c9650e63
Raffaele Balsamo <80632324+Raffaeleb003@users.noreply.github.com>
2023-05-01 10:37:43 +0200

    circumference and surface of circle

--- script.js
+++ script.js
@@ -13,3 +13,13 @@ const s = (sideA + sideB + sideC) / 2;
 const area = Math.sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
 
 console.log(`The area of a triangle with sides of length ${sideA}, ${sideB}, and ${sideC} is ${area}.`);
+
+
+
+const radius = 4;
+const pi = Math.PI;
+
+const circumference = 2 * pi * radius;
+const surfaceArea = pi * radius * radius;
+
+console.log(`A circle with radius ${radius} has a circumference of ${circumference.toFixed(2)} and a surface area of ${surfaceArea.toFixed(2)}.`);
